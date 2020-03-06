98d0446f0a1e85221e4f7db5d93dcadbdfaf2edb
Ciaran McCormick <ciaran.mccormick@itoworld.com>
2020-03-06 17:00:20 +0000

    Added more checks to exception checking

--- hootsweet/exceptions.py
+++ hootsweet/exceptions.py
@@ -15,10 +15,54 @@ class HootSuiteException(Exception):
 
 
 class BadRequest(HootSuiteException):
+    """400"""
+
+    pass
+
+
+class Unauthorized(HootSuiteException):
+    """401"""
+
+    pass
+
+
+class Forbidden(HootSuiteException):
+    """403"""
+
+    pass
+
+
+class NotFound(HootSuiteException):
+    """404"""
+
+    pass
+
+
+class TooManyRequests(HootSuiteException):
+    """429"""
+
+    pass
+
+
+class ServerError(HootSuiteException):
+    """500"""
+
     pass
 
 
 def detect_and_raise_error(response: Response):
-
-    if response.status_code == 400:
+    status_code = response.status_code
+    if status_code == 400:
         raise BadRequest(response)
+    elif status_code == 401:
+        raise Unauthorized(response)
+    elif status_code == 403:
+        raise Forbidden(response)
+    elif status_code == 404:
+        raise NotFound(response)
+    elif status_code == 429:
+        raise TooManyRequests(response)
+    elif status_code >= 500:
+        raise ServerError(response)
+    elif status_code >= 400:
+        raise BadRequest(Response)
