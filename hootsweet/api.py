344396c404ca5e34d3bcd41d2874817ddb7b2ed5
Ciaran McCormick <ciaran.mccormick@itoworld.com>
2020-03-06 16:38:53 +0000

    Added initial non functioning package

---
+++ hootsweet/api.py
@@ -0,0 +1,65 @@
+import requests
+
+from hootsweet.exceptions import BadRequest
+
+HOOTSUITE_BASE_URL = "https://platform.hootsuite.com/"
+API_VERSION = "v1"
+API_URL = "%s/%s" % (HOOTSUITE_BASE_URL, API_VERSION)
+
+
+class HootSweet:
+    def __init__(self, access_token=None, **kwargs):
+        self._access_token = access_token
+
+    def _make_request(self, method, resource, **kwargs):
+        url = "%s/%s" % (API_URL, resource)
+
+        headers = kwargs.get("headers", {})
+        headers.update({"Authorization": "Bearer %s" % self._access_token})
+        kwargs["headers"] = headers
+        response = requests.request(method, url, **kwargs)
+
+        if not response.status_code == 200:
+            raise BadRequest()
+        else:
+            return response.json()["data"]
+
+    def get_me(self):
+        """ Retrieves authenticated member"""
+        resource = "me"
+        return self._make_request("GET", resource)
+
+    def get_me_organisations(self):
+        """ Retrieves the organizations that the authenticated member is in."""
+        resource = "me/organisations"
+        return self._make_request("GET", resource)
+
+    def get_me_social_profiles(self):
+        """ Retrieves the social media profiles that the authenticated user has
+        BASIC_USAGE permissions on."""
+        resource = "me/socialProfiles"
+        return self._make_request("GET", resource)
+
+    def get_social_profiles(self):
+        """ Retrieves the social profiles that the authenticated user has access to."""
+        resource = "socialProfiles"
+        return self._make_request("GET", resource)
+
+    def get_social_profile(self, profile_id: int):
+        """Retrieve a social profile.
+
+        Requires BASIC_USAGE permission on the social profile.
+        """
+        resource = "socialProfiles/%s" % profile_id
+        return self._make_request("GET", resource)
+
+    def get_social_profile_teams(self, profile_id: int):
+        """Retrieves a list of team IDs with access to a social profile.
+
+        Requires BASIC_USAGE permission on the social profile or
+        ORG_MANAGE_SOCIAL_PROFILE permission on the organization that owns the
+        social profile.
+        """
+        resource = "socialProfiles/%s/teams" % profile_id
+        return self._make_request("GET", resource)
+
---
+++ hootsweet/exceptions.py
@@ -0,0 +1,26 @@
+from requests import Response
+
+
+class HootSuiteException(Exception):
+    def __init__(self, response, *args, **kwargs):
+        try:
+            errors = response.json()["errors"]
+            message = "{code} - {message}".format(**errors)
+
+        except Exception:
+            if hasattr(response, "status_code") and response.status_code == 401:
+                message = response.content.decode("utf-8")
+            else:
+                message = response
+        super().__init__(message, *args, **kwargs)
+
+
+class BadRequest(HootSuiteException):
+    pass
+
+
+def detect_and_raise_error(response: Response):
+
+    if response.status_code == 400:
+        raise BadRequest(response)
+    pass
